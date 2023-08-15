77f1dc21e4c258d52b367dac5ffe99a06d6d2849
jameschapman19 <jameschapman19@users.noreply.github.com>
2023-08-15 18:16:42 +0000

    Format code with black

--- test/test_tracenorm.py
+++ test/test_tracenorm.py
@@ -13,7 +13,6 @@ def test_tracenormprox():
     assert torch.trace(result) <= torch.trace(x)
 
 
-
 def test_tracenormconstraint():
     alpha = 0.1
     constraint = TraceNormConstraint(alpha=alpha)

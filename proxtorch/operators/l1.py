e603ff5383eadc82cf422ae9cf0ddf154cfa4f55
jameschapman19 <jameschapman19@users.noreply.github.com>
2023-08-15 10:35:35 +0000

    Format code with black

--- proxtorch/operators/l1.py
+++ proxtorch/operators/l1.py
@@ -39,4 +39,4 @@ class L1Prox(ProxOperator):
         Returns:
             float: The L1Prox norm value.
         """
-        return torch.sum(torch.abs(x))*self.sigma
+        return torch.sum(torch.abs(x)) * self.sigma

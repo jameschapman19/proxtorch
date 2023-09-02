4937dbc9f31abcfb6a2f9ee093add7c4586ffda1
jameschapman19 <james.chapman.19@ucl.ac.uk>
2023-09-02 11:42:21 +0100

    First commit

--- proxtorch/operators/tvl1_3d.py
+++ proxtorch/operators/tvl1_3d.py
@@ -1,3 +1,14 @@
+"""
+Total Variation L1 (TV-L1) denoising algorithm.
+
+Inspired by Nilearn's proxtvl1 implementation.
+
+References:
+- Wikipedia: Total Variation Denoising (http://en.wikipedia.org/wiki/Total_variation_denoising)
+- Beck, A., & Teboulle, M. (2009). Fast gradient-based algorithms for constrained total variation image denoising and deblurring problems.
+- Nilearn (https://nilearn.github.io/)
+"""
+
 from math import sqrt
 
 import torch
@@ -7,12 +18,25 @@ from proxtorch.base import ProxOperator
 
 
 def get_padding_tuple(dim_index, ndim):
+    """
+    Return a padding tuple for a specified dimension index.
+
+    Args:
+        dim_index (int): Index of the dimension for which padding is needed.
+        ndim (int): Total number of dimensions in the tensor.
+
+    Returns:
+        tuple: Padding tuple with a value of 1 at the specified dimension and 0 elsewhere.
+    """
     padding_tuple = [0] * (ndim * 2)
     padding_tuple[-2 * dim_index - 1] = 1
     return tuple(padding_tuple)
 
-
 class TVL1_3D(ProxOperator):
+    """
+    Class for the 3D Total Variation proximal operator.
+    """
+
     def __init__(
         self,
         alpha: float,
@@ -25,8 +49,9 @@ class TVL1_3D(ProxOperator):
 
         Args:
             alpha (float): Regularization strength.
-            max_iter (int, optional): Maximum iterations for the iterative algorithm. Defaults to 50.
-            tol (float, optional): Tolerance level for early stopping. Defaults to 1e-2.
+            max_iter (int, optional): Maximum iterations for the iterative algorithm. Defaults to 200.
+            tol (float, optional): Tolerance level for early stopping. Defaults to 1e-4.
+            l1_ratio (float, optional): The L1 ratio. Defaults to 0.0.
         """
         super().__init__()
         self.alpha = alpha
@@ -35,21 +60,35 @@ class TVL1_3D(ProxOperator):
         self.l1_ratio = l1_ratio
 
     def gradient(self, x):
+        """
+        Compute the gradient of the tensor x using finite differences.
+
+        Args:
+            x (torch.Tensor): Input tensor.
+
+        Returns:
+            torch.Tensor: Gradients of the tensor x.
+        """
         gradients = torch.zeros((x.dim() + 1,) + x.shape, device=x.device)
-        # for each dimension compute the gradient using torch.diff
+        # For each dimension compute the gradient using torch.diff
         for d in range(x.dim()):
-            # torch.diff outputs a tensor with one less element along the specified dimension
-            # we pad the output with zeros to match the shape of the input
             gradients[d, ...] = F.pad(
                 torch.diff(x, dim=d, n=1), pad=get_padding_tuple(d, x.dim())
             )
-
         gradients[:-1] *= 1.0 - self.l1_ratio
-
         gradients[-1] = self.l1_ratio * x
         return gradients
 
     def divergence(self, p: torch.Tensor) -> torch.Tensor:
+        """
+        Compute the divergence of the tensor p.
+
+        Args:
+            p (torch.Tensor): Input tensor.
+
+        Returns:
+            torch.Tensor: Divergence of the tensor p.
+        """
         div_x = torch.zeros_like(p[-1])
         div_y = torch.zeros_like(p[-1])
         div_z = torch.zeros_like(p[-1])
@@ -69,13 +108,19 @@ class TVL1_3D(ProxOperator):
         return (div_x + div_y + div_z) * (1 - self.l1_ratio) - self.l1_ratio * p[-1]
 
     def _projector_on_tvl1_dual(self, grad):
-        """Function to compute TV-l1 duality gap.
+        """
+        Function to compute TV-l1 duality gap.
 
         Modifies IN PLACE the gradient + id to project it
         on the l21 unit ball in the gradient direction and the L1 ball in the
         identity direction.
-        """
 
+        Args:
+            grad (torch.Tensor): Gradient tensor.
+
+        Returns:
+            torch.Tensor: Projected gradient tensor.
+        """
         # The l21 ball for the gradient direction
         if self.l1_ratio < 1.0:
             # infer number of axes and include an additional axis if l1_ratio > 0
@@ -95,9 +140,20 @@ class TVL1_3D(ProxOperator):
 
     def _dual_gap_prox_tvl1(self, input_img_norm, new, gap, weight, l1_ratio=1.0):
         """
-        Dual gap of total variation denoising
-        see "Total variation regularization for fMRI-based prediction of behavior",
-        by Michel et al. (2011) for a derivation of the dual gap
+        Compute the dual gap of total variation denoising.
+
+        Args:
+            input_img_norm (float): Norm of the input image.
+            new (torch.Tensor): Updated tensor.
+            gap (torch.Tensor): Gap tensor.
+            weight (float): Regularization strength.
+            l1_ratio (float, optional): The L1 ratio. Defaults to 1.0.
+
+        Returns:
+            float: Dual gap value.
+        Notes:
+            see "Total variation regularization for fMRI-based prediction of behavior",
+            by Michel et al. (2011) for a derivation of the dual gap
         """
         tv_new = self.tvl1_from_grad(self.gradient(new))
         gap = gap.view(-1)
@@ -110,7 +166,7 @@ class TVL1_3D(ProxOperator):
         return 0.5 * d_gap
 
     def prox(self, x: torch.Tensor, lr: float) -> torch.Tensor:
-        r"""
+        """
         Iterative algorithm to compute the proximal mapping of the tensor.
 
         Args:
@@ -119,56 +175,27 @@ class TVL1_3D(ProxOperator):
 
         Returns:
             torch.Tensor: Tensor after applying the proximal operation.
-        """
-        fista = True
-        weight = self.alpha * lr
-        input_shape = x.shape
-        input_img_norm = torch.norm(x) ** 2
-        lipschitz_constant = 1.1 * (4 * 3)
-        negated_output = -x
-        output = torch.zeros_like(x)
-        grad_aux = torch.zeros_like(self.gradient(x))
-        grad_im = torch.zeros_like(grad_aux)
-        t = 1.0
-        i = 0
-        old_dgap = torch.tensor(float("inf")).to(x.device)
-        dgap = torch.tensor(float("inf")).to(x.device)
-        while i < self.max_iter:
-            # tv_prev = self.tv_from_grad(self.gradient(output))
-            grad_tmp = self.gradient(negated_output)
-            grad_tmp *= 1.0 / (lipschitz_constant * weight)
-            grad_aux += grad_tmp
-            grad_tmp = self._projector_on_tvl1_dual(grad_aux)
 
-            # Careful, in the next few lines, grad_tmp and grad_aux are a
-            # view on the same array, as _projector_on_tvl1_dual returns a view
-            # on the input array
-            t_new = 0.5 * (1 + sqrt(1 + 4 * t**2))
-            t_factor = (t - 1) / t_new
-            if fista:
-                # fista
-                grad_aux = (1 + t_factor) * grad_tmp - t_factor * grad_im
-            else:
-                # ista
-                grad_aux = grad_tmp
-            grad_im = grad_tmp
-            t = t_new
-            gap = weight * self.divergence(grad_aux)
-            negated_output = gap - x
-            if i % 4 == 0:
-                old_dgap = dgap
-                dgap = self._dual_gap_prox_tvl1(
-                    input_img_norm, -negated_output, gap, weight, l1_ratio=self.l1_ratio
-                )
-                if dgap < 5.0e-5:
-                    break
-                if old_dgap < dgap:
-                    fista = False
-                elif fista:
-                    fista = True
-            i += 1
-        output = x - weight * self.divergence(grad_im)
-        return output.reshape(input_shape)
+        Notes
+        -----
+        The principle of total variation denoising is explained in
+        http://en.wikipedia.org/wiki/Total_variation_denoising
+
+        The principle of total variation denoising is to minimize the
+        total variation of the image, which can be roughly described as
+        the integral of the norm of the image gradient. Total variation
+        denoising tends to produce "cartoon-like" images, that is,
+        piecewise-constant images.
+
+        This function implements the FISTA (Fast Iterative Shrinkage
+        Thresholding Algorithm) algorithm of Beck et Teboulle, adapted to
+        total variation denoising in "Fast gradient-based algorithms for
+        constrained total variation image denoising and deblurring problems"
+        (2009).
+
+        For details on implementing the bound constraints, read the aforementioned
+        Beck and Teboulle paper.
+        """
 
     def _nonsmooth(self, x: torch.Tensor) -> torch.Tensor:
         """
